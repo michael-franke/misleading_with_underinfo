f2c86a32414246dbad33d2478cca601c7f8b66d3
michael-franke <mchfranke@gmail.com>
2019-03-16 09:31:00 +0100

    TVJ added

---
+++ experiments/02_TVJ_pretest/main.js
@@ -0,0 +1,45 @@
+// initialises a babe experiment with babeInit
+$("document").ready(function() {
+    // prevent scrolling when space is pressed
+    window.onkeydown = function(e) {
+        if (e.keyCode == 32 && e.target == document.body) {
+            e.preventDefault();
+        }
+    };
+
+    // calls babeInit
+    babeInit({
+        views_seq: [
+            intro,
+            // instructions_general,
+            // instructions_color_blindness,
+            // color_blindness_test,
+            // instructions_part1,
+            // comprehension_test,
+            // sentence_completion,
+            // instructions_self_assessment,
+            // performance_rating,
+            instructions_part2,
+            truth_value_judgements,
+            instructionsPostTest,
+            post_test,
+            thanks,
+        ],
+        deploy: {
+            experimentID: "42",
+            serverAppURL: "https://mcmpact.ikw.uni-osnabrueck.de/babe/api/submit_experiment/42",
+            deployMethod: "prolific",
+            contact_email: "the.secret.goldfish@me.com",
+            prolificURL: "test"
+            
+        },
+        progress_bar: {
+            in: [
+                // list the view-names of the views for which you want a progress bar
+                "truth_value_judgements"
+            ],
+            style: "separate",
+            width: 100
+        },
+    });
+});
