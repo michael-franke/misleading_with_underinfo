b733f56803d52591e9d1b57395da8c85d0c870ac
michael-franke <mchfranke@gmail.com>
2019-03-26 14:01:49 +0100

    preparing for prime time pilot main exp

--- experiments/04_forced_choice/main.js
+++ experiments/04_forced_choice/main.js
@@ -12,12 +12,12 @@ $("document").ready(function() {
         views_seq: [
             intro,
             instructions_general,
-            // instructions_color_blindness,
-            // color_blindness_test,
+            instructions_color_blindness,
+            color_blindness_test,
             instructions_part1,
+            comprehension_test,
             wait_for_player,
-            // comprehension_test,
-            // sentence_completion,
+            sentence_completion,
             instructions_self_assessment,
             performance_rating,
             instructions_part2,
@@ -27,11 +27,11 @@ $("document").ready(function() {
             thanks,
         ],
         deploy: {
-            experimentID: "INSERT_A_NUMBER",
-            serverAppURL: "https://babe-demo.herokuapp.com/api/submit_experiment/",
-            deployMethod: "debug",
-            contact_email: "YOUREMAIL@wherelifeisgreat.you",
-            prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
+            experimentID: "46",
+            serverAppURL: "https://mcmpact.ikw.uni-osnabrueck.de/babe/api/submit_experiment/",
+            deployMethod: "Prolific",
+            contact_email: "exprag@gmail.com",
+            prolificURL: "https://app.prolific.ac/submissions/complete?cc=ADL8OZB3"
         },
         progress_bar: {
             in: [
