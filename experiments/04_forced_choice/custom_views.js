191ab1f366e852d66d6d32e3fea99167d80e5ba1
michael-franke <mchfranke@gmail.com>
2019-03-26 20:34:28 +0100

    reduced trial number for testing

--- experiments/04_forced_choice/custom_views.js
+++ experiments/04_forced_choice/custom_views.js
@@ -480,8 +480,8 @@ const forcedChoice_pause = function(config) {
 }
 
 const sentence_completion = sentence_completion_type({
-    // trials: 2,
-    trials: main_trials.sentence_completion.length,
+    trials: 7,
+    // trials: main_trials.sentence_completion.length,
     name: 'sentence_completion',
     trial_type: 'sentence_completion',
     data: _.shuffle(main_trials.sentence_completion)
@@ -506,8 +506,8 @@ const performance_rating = slider_rating_custom_type({
 });
 
 const truth_value_judgements = forcedChoice_pause({
-    // trials: 2,
-    trials: main_trials.truth_value_judgements.length,
+    trials: 7,
+    // trials: main_trials.truth_value_judgements.length,
     name: "truth_value_judgements",
     trial_type: "truth_value_judgements",
     title: "True or false?",
